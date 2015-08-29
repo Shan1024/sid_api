c272c551ad1aeb5d91bc2e4fc8a10f6facc16c92
Shan1024 <gambit1024@gmail.com>
2015-08-30 00:20:52 +0530

    New user schema created

--- app/models/user.js
+++ app/models/user.js
@@ -4,6 +4,41 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('User', new Schema({
-    name: String,
-    password: String
+	user:{
+		local:{
+			username: String,
+			password: String
+		},
+		facebook:{
+			id: String,
+			name: String,
+			email: String,
+			token: String
+		},
+		linkedin:{
+			id: String,
+			name: String,
+			email: String,
+			token: String
+		}
+	},
+	facebook:{
+		ratedByMe:[
+			{
+				id: String,
+				content: String,
+				rating: String
+			}
+		],
+		myRatings:[
+			{
+				id: String,
+				content: String,
+				rating: String
+			}
+		]
+	},
+	linkedin:{
+
+	}
 }));
