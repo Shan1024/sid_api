d0703fdb790a5ee69b09e47396146311d0c387bb
Shan1024 <gambit1024@gmail.com>
2015-09-15 20:20:32 +0530

    Code formatted

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,39 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      all: ['*.js', 'socialconfig/*.js','app/*.js', 'app/models/*.js', 'test/*.js']
-    },
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec',
-          captureFile: 'results.txt', // Optionally capture the reporter output to a file
-          quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+    grunt.initConfig({
+        jshint: {
+            all: ['*.js', 'socialconfig/*.js', 'app/**/*.js', 'test/*.js']
         },
-        src: ['test/**/*.js']
-      }
-    },
-    apidoc: {
-      myapp: {
-        src: "app/",
-        dest: "public/apidoc/",
-        options: {
-          debug: true,
-          includeFilters: [ ".*\\.js$" ],
-          excludeFilters: [ "node_modules/" ]
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['test/**/*.js']
+            }
+        },
+        apidoc: {
+            myapp: {
+                src: "app/",
+                dest: "public/apidoc/",
+                options: {
+                    debug: true,
+                    includeFilters: [".*\\.js$"],
+                    excludeFilters: ["node_modules/"]
+                }
+            }
         }
-      }
-    }
-  });
+    });
     // "test": "mocha --reporter spec"
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-apidoc');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-apidoc');
 
-  grunt.registerTask('default', ['jshint','apidoc']);//,'mochaTest'
+    grunt.registerTask('default', ['jshint']); //,'mochaTest','apidoc'
 
-  // grunt.registerTask('default', ['jshint']);
+    // grunt.registerTask('default', ['jshint']);
 
 };
